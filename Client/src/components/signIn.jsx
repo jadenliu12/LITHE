58f5bd5379a0eae88c40ec40082cab5e0f829849
ginagigo123 <ginagigo123@gmail.com>
2021-06-05 18:03:42 +0800

    update signIn and signUp

--- Client/src/components/signIn.jsx
+++ Client/src/components/signIn.jsx
@@ -1,4 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, useHistory, useLocation } from 'react-router-dom';
+import { 
+    Button,
+    Form,
+    FormGroup,
+    Label,
+    Input
+} from "reactstrap";
+
+import './sign.css';
+import SignUp from "./signUp.jsx";
 
 export default class SignIn extends React.Component {
 
@@ -9,7 +20,7 @@ export default class SignIn extends React.Component {
             loading: true,
             masking: true
         };
-        
+        this.signin = this.signin.bind(this);
     }
 
     componentDidMount() {
@@ -20,11 +31,47 @@ export default class SignIn extends React.Component {
         console.log('Unmount Sign In')
     }
 
+    signin() {
+        // let history = useHistory();
+        // let location = useLocation();
+        
+        // console.log("Histroy", history);
+        // console.log("location", location);
+    }
+
     render() {
         return (
-            <div className={`signIn`}>
+            <div className="signIn">
                 <div className={`mask ${this.state.masking ? 'masking' : ''}`}>
-                    <h1>THIS IS SIGN IN</h1>
+                    <Form className="sign-form">
+                        <h1 className="text-info">Lithe.</h1>
+                        <h3 className="text-center">Welcome</h3>
+                        <FormGroup>
+                            <Label>Email:</Label>
+                            <Input type="email" placeholder="Email"></Input>
+                        </FormGroup>
+
+                        <FormGroup>
+                            <Label>Password:</Label>
+                            <Input type="password" placeholder="Password"></Input>
+                        </FormGroup>
+                        <Button className="btn-lg btn-block" onClick={this.signin}>Sign In</Button>
+
+                        <div className="text-center">
+                            <Link to="/signUp" className="text-muted">Not register? create an account</Link>
+                        </div>
+                        <div className="text-center">
+                            <Link to="/signUp" className="text-muted">Forget your password?</Link>
+                        </div>
+
+                        <Route
+                            exact
+                            path="/signUp"
+                            render={() => (
+                            <SignUp/>
+                            )}
+                        />
+                    </Form>
                 </div>
             </div>
         );
