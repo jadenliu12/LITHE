013d607c24a9969adb9eda20ffca93743d456c46
108062281 <mnhartono@gmail.com>
2021-06-25 20:05:12 +0800

    heatmap

--- Client/src/components/home.jsx
+++ Client/src/components/home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Features from 'components/features.jsx'
+import CalendarHeatMap from 'components/calendarHeatmap.jsx';
 
 import './home.css';
 
@@ -12,7 +12,6 @@ export default class Home extends React.Component {
             loading: false,
             masking: false
         };
-        
     }
 
     componentDidMount() {
@@ -25,16 +24,25 @@ export default class Home extends React.Component {
 
     render() {
         return (
-            <div className="webHome">
-                <div className="top">                
-                    <h1>LITHE</h1>
-                    <h3>Goals are met, friends are made.</h3>
-                    <p>Meet friends with the same goal along your journey in maintaining a healthy diet. Compete, Share, and Motivate each other along the way.</p>      
-                    <button>Start</button>                              
+            <div className="calendar">
+                <h1>Statistics</h1>
+                <h5>150 days</h5>
+                <div className="heatmap"><CalendarHeatMap /></div>
+                <div className="colorIndicator">
+                    <p>Target Indicators</p>
+                    <div className="colors">
+                        <inline className="color0"></inline>
+                        <inline>0% - 20%</inline>
+                        <inline className="color1"></inline>
+                        <inline>21% - 40%</inline>
+                        <inline className="color2"></inline>
+                        <inline>41% - 60%</inline>
+                        <inline className="color3"></inline>
+                        <inline>61% - 80%</inline>
+                        <inline className="color4"></inline>
+                        <inline>81% - 100%</inline>
+                    </div>
                 </div>
-                <div className="middle">
-                    <Features/>
-                </div>              
             </div>        
         );
     }
