154ec5b924f74922898f3eed58cedd2b751dc22e
108062281 <mnhartono@gmail.com>
2021-06-27 01:09:26 +0800

    update

--- Client/src/components/aboutUs.jsx
+++ Client/src/components/aboutUs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ContactUs from 'components/contactUs.jsx'
+import ChatBar from 'components/chatBar.jsx'
 
 import './aboutUs.css';
 
@@ -7,7 +7,7 @@ export default class AboutUs extends React.Component {
 
     constructor(props) {
         super(props);
-
+        this.toggleChat = 0;
         this.state = {            
             loading: false,
             masking: false
@@ -23,6 +23,11 @@ export default class AboutUs extends React.Component {
         console.log('Unmount About Us')
     }
 
+    toggleChatWindow() {
+        this.toggleChat = ~this.toggleChat;
+        ChatBar.openChat(this.toggleChat);
+    }
+
     render() {
         return (
             <div className="aboutUs">
@@ -36,7 +41,7 @@ export default class AboutUs extends React.Component {
                             <div className="col-sm"><i className="fas fa-plus-circle fa-3x"></i></div>
                         </div>
                         <div className="row">
-                            <p className="col-sm">user1</p>
+                            <p className="col-sm"><img src="images/h1300.png"></img></p>
                             <p className="col-sm">user2</p>
                             <p className="col-sm">user3</p>
                             <p className="col-sm">user4</p>
@@ -56,6 +61,12 @@ export default class AboutUs extends React.Component {
                         </div>
                     </div>
                 </div>
+
+                <form className="chat">
+                    <div className="start-chat">
+                        <i onClick={this.toggleChatWindow} className="fas fa-comments"></i>
+                    </div>
+                </form>
             </div>        
         );
     }
