9d334554efda4d82dc3f40b88e022e944f342f5b
Jaden <Jaden.1012@yahoo.com>
2021-06-28 22:39:26 +0800

    update backend

--- Server/src/model/updateUserInfo.js
+++ Server/src/model/updateUserInfo.js
@@ -7,7 +7,7 @@ function create(username, cal, sleep, water) {
     const sql = `
             UPDATE usersinfo
             SET calories = $2, sleep = $3, water = $4
-            WHERE username = $1:name
+            WHERE username = '$1:value'
             RETURNING *
         `;
     return db.one(sql, [username, cal, sleep, water]);
